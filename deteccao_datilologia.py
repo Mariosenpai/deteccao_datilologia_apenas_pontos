25a14f37420c4dd738ae2d54251936f0fd3202d9
Mario Freitas <mario.freitas@nca.ufma.br>
2023-07-04 11:01:27 -0300

    add adiciona normalização dos pontos entre 1 e 0, adiciona um imageBox normalizado

--- deteccao_datilologia.py
+++ deteccao_datilologia.py
@@ -1,13 +1,14 @@
 import cv2
 import time
 import pickle
+from funcoes import enquadra_mao, normalizar_pontos
 
 from cvzone.HandTrackingModule import HandDetector
 
 cap = cv2.VideoCapture(0)
-detector = HandDetector(maxHands =1)
+detector = HandDetector(maxHands=1)
 
-#quantidade de frame que ira salva
+# quantidade de frame que ira salva
 qnt_frame = 30
 
 cont_frame = 0
@@ -19,34 +20,51 @@ pontos_mao = []
 qnt_salvos = 0
 
 while True:
-    suc,img = cap.read()
+    suc, img = cap.read()
     hands, img = detector.findHands(img)
+    enquadra_mao(hands, img)
 
     cv2.imshow("Image", img)
     key = cv2.waitKey(1)
-    
-    #inicia a gravação
-    if key == ord('s'):        
+
+    # inicia a gravação
+    if key == ord('s'):
         gravando = not gravando
         print(f"Gravando = {gravando}")
-       
+
     if gravando:
-        
+
         if cont_frame >= qnt_frame:
             gravando = False
-            with open(f'letras/C/{time.time()}.pickle', 'wb') as arquivo:
+            with open(f'letras/A/{time.time()}.pickle', 'wb') as arquivo:
                 pickle.dump(pontos_mao, arquivo)
+                print(pontos_mao)
             arquivo.close()
-            
+
             qnt_salvos += 1
             cont_frame = 0
             pontos_mao = []
-            
+
             print(f"Gravando = {gravando}")
             print(f"Quantidade salvos = {qnt_salvos}")
-         
+
         if hands:
+            x, y, h, w = enquadra_mao(hands, img)
             hand = hands[0]
-            pontos_mao += hand['lmList']
+            pontos_frame = hand['lmList']
+
+            for ponto in pontos_frame:
+
+                p_x = ponto[0]
+                p_y = ponto[1]
+
+                p_x = p_x - x
+                p_y = p_y - y
+
+                ponto[0] = p_x
+                ponto[1] = p_y
+
+            pontos_frame = normalizar_pontos(pontos_frame)
+
+            pontos_mao.append(pontos_frame)
             cont_frame += 1
-                
--- funcoes.py
+++ funcoes.py
@@ -1,26 +1,73 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
 from tensorflow.keras.callbacks import TensorBoard
+import cv2
 import os
+import numpy as np
+import math
 
 
-
-
-def enquadra_mao(hands):
+def enquadra_mao(hands, img):
     add_espaco = 20
+    tam_img = 300
+
     if hands:
-        #evita erro de tive apenas uma parte da mao na tela
+        # evita erro de tive apenas uma parte da mao na tela
         try:
             hand = hands[0]
-            x,y,w,h = hand['bbox']
-            imgCrop = img[y - add_espaco: y+h +add_espaco, x-add_espaco: x+w + add_espaco]
-            cv2.imshow("imageMao", imgCrop)
+            x, y, w, h = hand['bbox']
+
+            imgBranca = np.ones((tam_img, tam_img, 3), np.uint8) * 255
+            imgBox = img[y - add_espaco: y + h + add_espaco, x - add_espaco: x + w + add_espaco]
+            #cv2.imshow("imageMao", imgBox)
+
+            imgBoxShape = imgBox.shape
+
+            proporcao_tela = h / w
+
+            if proporcao_tela > 1:
+                k = tam_img / h
+                w_cal = math.ceil(k * w)
+                img_resize = cv2.resize(imgBox, (w_cal, tam_img))
+                img_resize_shape = img_resize.shape
+                w_gap = math.ceil((tam_img - w_cal) / 2)
+                imgBranca[:, w_gap:w_cal + w_gap] = img_resize
+            else:
+                k = tam_img / w
+                h_cal = math.ceil(k * h)
+                img_resize = cv2.resize(imgBox, (tam_img, h_cal))
+                img_resize_shape = img_resize.shape
+                h_gap = math.ceil((tam_img - h_cal) / 2)
+                imgBranca[h_gap:h_cal + h_gap, :] = img_resize
+
+            cv2.imshow("img_tam_fixo", imgBranca)
+
+            return x, y, w, h
         except:
-            pass 
+            pass
+
+def normalizar_pontos(pontos_frame):
+
+    maior_x = 0
+    maior_y = 0
+    maior_z = 0
+    for ponto in pontos_frame:
+        if maior_x > ponto[0]:
+            maior_x = ponto[0]
+        if maior_y > ponto[1]:
+            maior_y = ponto[1]
+        if maior_z > ponto[2]:
+            maior_z = ponto[2]
+
+    for ponto in pontos_frame:
+        ponto[0] = ponto[0] / maior_x
+        ponto[1] = ponto[1] / maior_y
+        ponto[2] = ponto[2] / maior_z
+
+    return pontos_frame
 
 
 def model(shape):
-    
     model = Sequential()
     model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=shape))
     model.add(LSTM(128, return_sequences=True, activation='relu'))
@@ -28,18 +75,18 @@ def model(shape):
     model.add(Dense(64, activation='relu'))
     model.add(Dense(32, activation='relu'))
     model.add(Dense(3, activation='softmax'))
-    
+
     model.summary()
-    
+
     model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
-    
+
     return model
 
+
 def pegaCaminhoArquivos(pasta_principal):
-    
     caminho_dic = {}
     lista_arquivos = []
-    
+
     # Itera sobre os diretórios dentro da pasta principal
     for diretorio in os.listdir(pasta_principal):
         # Verifica se o item é um diretório
@@ -51,9 +98,9 @@ def pegaCaminhoArquivos(pasta_principal):
                 # Verifica se o item é um arquivo
                 if os.path.isfile(os.path.join(caminho_diretorio, arquivo)):
                     # Imprime o caminho completo do arquivo
-                    caminho_arquivo = os.path.join(caminho_diretorio, arquivo) 
-                    lista_arquivos.append(caminho_arquivo)               
+                    caminho_arquivo = os.path.join(caminho_diretorio, arquivo)
+                    lista_arquivos.append(caminho_arquivo)
         caminho_dic.update({diretorio: lista_arquivos})
         lista_arquivos = []
-        
+
     return caminho_dic
